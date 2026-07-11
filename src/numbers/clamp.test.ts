42d14d036b395a40db2c6c51186f3476ae4ec97a
Jason Godesky <jefgodesky@gmail.com>
2026-07-11 13:31:00 -0400

    Add new method: clamp

---
+++ src/numbers/clamp.test.ts
@@ -0,0 +1,16 @@
+import { describe, it, expect } from 'vitest'
+import clamp from './clamp.ts'
+
+describe('clamp', () => {
+  it('returns min if given a value too small', () => {
+    expect(clamp(1, 2, 3)).toBe(2)
+  })
+
+  it('returns the value if between min and max', () => {
+    expect(clamp(2, 1, 3)).toBe(2)
+  })
+
+  it('returns the max if given a value too big', () => {
+    expect(clamp(3, 1, 2)).toBe(2)
+  })
+})
---
+++ src/numbers/clamp.ts
@@ -0,0 +1,9 @@
+const clamp = (
+  val: number,
+  min: number,
+  max: number
+): number => {
+  return Math.max(min, Math.min(val, max))
+}
+
+export default clamp
