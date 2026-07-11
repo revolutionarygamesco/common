90fb603d4909d3ef6d0ea4addffc6a92622922e7
Jason Godesky <jefgodesky@gmail.com>
2026-07-11 13:19:02 -0400

    selectRandomBetween: Refactor tests to use isWithinRange

--- src/randomizers/between.test.ts
+++ src/randomizers/between.test.ts
@@ -1,26 +1,9 @@
-import { describe, beforeEach, it, expect } from 'vitest'
+import { describe, it, expect } from 'vitest'
+import isWithinRange from '../numbers/range.ts'
 import selectRandomBetween from './between.ts'
 
 describe('selectRandomBetween', () => {
-  let actual: number = NaN
-
-  beforeEach(() => {
-    actual = selectRandomBetween(1, 10)
-  })
-
-  it('does not return a number lower than the range', () => {
-    expect(0).toBeLessThan(actual)
-  })
-
-  it('returns a number greater than or equal to the bottom of the range', () => {
-    expect(1).toBeLessThanOrEqual(actual)
-  })
-
-  it('returns a number less than or equal to the top of the range', () => {
-    expect(10).toBeGreaterThanOrEqual(actual)
-  })
-
-  it('does not return a number greater than the range', () => {
-    expect(11).toBeGreaterThan(actual)
+  it('returns a random number between min and max', () => {
+    expect(isWithinRange(selectRandomBetween(1, 10), [1, 10]))
   })
 })
