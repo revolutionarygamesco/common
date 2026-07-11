f70578b64f7d6fa575d8dbc76877f9234b2c13c5
Jason Godesky <jefgodesky@gmail.com>
2026-07-11 13:33:28 -0400

    Add new method: radiansToDegrees

---
+++ src/numbers/rad-deg.test.ts
@@ -0,0 +1,14 @@
+import { describe, it, expect } from 'vitest'
+import radiansToDegrees from './rad-deg.ts'
+
+describe('radiansToDegrees', () => {
+  it.each([
+    [0, 0],
+    [1.57, 89.95],
+    [3.14, 179.91],
+    [4.71, 269.86],
+    [6.28, 359.82]
+  ] as [number, number][])('converts %d radians to %d°', (radians, degrees) => {
+    expect(radiansToDegrees(radians)).toBeCloseTo(degrees)
+  })
+})
---
+++ src/numbers/rad-deg.ts
@@ -0,0 +1,5 @@
+const radiansToDegrees = (
+  radians: number
+): number => radians * (180 / Math.PI)
+
+export default radiansToDegrees
