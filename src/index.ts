851c949c1369eb0cecad4fac4322271684679163
Jason Godesky <jefgodesky@gmail.com>
2026-07-11 13:51:27 -0400

    Create tsdown config + barrel

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+export * from './guards/array.ts'
+export * from './guards/number.arr.ts'
+export * from './guards/number.ts'
+export * from './guards/object.ts'
+export * from './guards/string.arr.ts'
+export * from './guards/string.ts'
+export * from './guards/string-union.ts'
+export * from './guards/tuple.ts'
+
+export * from './numbers/clamp.ts'
+export * from './numbers/deg-rad.ts'
+export * from './numbers/rad-deg.ts'
+export * from './numbers/range.ts'
+
+export * from './randomizers/between.ts'
+export * from './randomizers/el.ts'
+export * from './randomizers/shuffle.ts'
+
+export * from './strings/capital.ts'
+export * from './strings/decapital.ts'
+export * from './strings/oxford.ts'
+export * from './strings/union.ts'
+
+export * from './testing/primitives.ts'
+
+export * from './stock.ts'
